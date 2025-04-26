ede60918b304974698405ba50bd8e656cbca506f
Samiul Islam <39089110+saad85@users.noreply.github.com>
2025-04-26 20:27:18 +0600

    Added all files.

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# portfolio-NextJS
+# Portfolio
---
+++ data/projects.ts
@@ -0,0 +1,45 @@
+export const projects = [
+  {
+    id: 6,
+    image: "uniteliving.png",
+    title: "Uniteliving (version 1) - Full Stack Developer",
+    description:
+      "Worked as a Full Stack Developer at Gain Solutions Ltd, where I led the development of Uniteliving v1, a real estate management platform. The application was built using Meteor.js, Node.js, Vue.js, MongoDB, and Mongoose, with deployment and infrastructure managed through AWS services.",
+  },
+  {
+    id: 2,
+    image: "uniteliving.png",
+    title: "Uniteliving (version 2) - Backend Developer",
+    description:
+      "Worked as a Backend Developer at Gain Solutions Ltd, I built and maintained the backend architecture for Uniteliving, a real estate management platform. Developed using Express.js, GraphQL, MongoDB, and Mongoose ORM, with deployment and services powered by AWS.Key responsibilities included designing and implementing core modules such as the transaction and payout system. Integrated third-party libraries and services including Google Geolocation APIs and IDFY for location and identity verification. Ensured scalable, secure, and efficient backend operations to support platform growth.",
+  },
+  {
+    id: 1,
+    image: "swivflow2.png",
+    title: "SwivFlow – Full Stack Developer",
+    description: `Worked as a Full Stack Developer at Aimsbay Solutions, Led the full stack development of SwivFlow, a collaborative project and task management platform. Built using Next.js, Node.js, Express.js, GraphQL, PostgreSQL, and Prisma ORM, with infrastructure deployed on AWS.
+Developed key modules including Projects, Task Management System, and other productivity features. Integrated third-party services such as Vonage for messaging and AWS S3 for secure file storage and retrieval. Focused on building a scalable, responsive, and user-friendly application from front to back.
+
+`,
+  },
+  {
+    id: 3,
+    image: "payrun.png",
+    title: "Payrun – Backend Developer",
+    description: `Worked as a Backend Developer at Gain Solutions Ltd, Developed and maintained the backend of Payrun, a comprehensive HR and payroll management system. Built using Express.js, GraphQL, PostgreSQL, and Sequelize ORM, with deployment and services managed on AWS.
+Designed a highly optimized and scalable database architecture to efficiently handle large volumes of data. Implemented key modules including Timeline, Leave Management, Hiring System, and Data Import workflows. Focused on performance, data integrity, and scalability to support enterprise-level operations.`,
+  },
+  {
+    id: 4,
+    image: "easydesk.png",
+    title: "Easydesk – Backend Developer",
+    description: `Contributed to the backend development of Easydesk, a customer support integration platform. Utilized Express.js, GraphQL, PostgreSQL, and Sequelize ORM, with services deployed on AWS.
+Designed a scalable and optimized database architecture to handle large-scale data efficiently. Developed robust systems for importing and synchronizing data from third-party platforms such as Freshdesk and Zendesk, ensuring data consistency and high performance under heavy load.`,
+  },
+  {
+    id: 5,
+    image: "smartsitesensor.png",
+    title: "Smart Site Sensor – AI-Powered Chat App (Personal Project)",
+    description: `Developed a personal project using Next.js, and Upstash platform. Built a chat interface that allows users to interact with AI-powered responses. Utilized React for the frontend and Tailwind CSS for styling. Implemented real-time chat functionality and message history. Ensured responsive design and smooth user experience.`,
+  },
+];
